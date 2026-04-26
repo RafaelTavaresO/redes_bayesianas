c741caabc93ee154acc86e2c520dce7479a264c8
RafaelTavaresO <rafael25_tavares@hotmail.com>
2026-04-26 20:54:21 -0300

    Atualização 2.0 - Temp3pm

--- data_clean.py
+++ data_clean.py
@@ -49,9 +49,9 @@ df_rede['Rainfall'] = pd.cut(
     bins=[-0.1, 1, 10, 50, 1000],
     labels=[
         'Sem Chuva',
-        'Acumulo Leve',
-        'Acumulo Médio',
-        'Acumulo Alto'
+        'Preciptação Baixa',
+        'Preciptação Média',
+        'Preciptação Alta'
     ]
 )
 
